0949ae3f0c228b2bd53d44871b510f6c76b81efa
Joakim Hellsén <tlovinator@gmail.com>
2022-07-06 17:22:20 +0200

    Add get_following()

--- tests/test_user.py
+++ tests/test_user.py
@@ -134,3 +134,71 @@ def test_get_followers():
             assert type(follower.restricted) is bool
 
             assert hasattr(follower, "starred_repos_count")
+
+            assert hasattr(follower, "visibility")
+            assert type(follower.visibility) is str
+
+            assert hasattr(follower, "website")
+            assert type(follower.website) is str
+
+
+def test_get_following():
+    with Gitea(url=pytest.url, token=pytest.token, log_level="DEBUG") as gitea:
+        following = User.get_following(gitea, page=1, limit=10)
+
+        for user in following:
+            assert hasattr(user, "active")
+            assert type(user.active) is bool
+
+            assert hasattr(user, "avatar_url")
+            assert type(user.avatar_url) is str
+
+            assert hasattr(user, "created")
+            assert type(user.created) is str
+
+            assert hasattr(user, "description")
+            assert type(user.description) is str
+
+            assert hasattr(user, "email")
+            assert type(user.email) is str
+
+            assert hasattr(user, "followers_count")
+            assert type(user.followers_count) is int
+
+            assert hasattr(user, "following_count")
+            assert type(user.following_count) is int
+
+            assert hasattr(user, "full_name")
+            assert type(user.full_name) is str
+
+            assert hasattr(user, "id")
+            assert type(user.id) is int
+
+            assert hasattr(user, "is_admin")
+            assert type(user.is_admin) is bool
+
+            assert hasattr(user, "language")
+            assert type(user.language) is str
+
+            assert hasattr(user, "last_login")
+            assert type(user.last_login) is str
+
+            assert hasattr(user, "location")
+            assert type(user.location) is str
+
+            assert hasattr(user, "login")
+            assert type(user.login) is str
+
+            assert hasattr(user, "prohibit_login")
+            assert type(user.prohibit_login) is bool
+
+            assert hasattr(user, "restricted")
+            assert type(user.restricted) is bool
+
+            assert hasattr(user, "starred_repos_count")
+
+            assert hasattr(user, "visibility")
+            assert type(user.visibility) is str
+
+            assert hasattr(user, "website")
+            assert type(user.website) is str
